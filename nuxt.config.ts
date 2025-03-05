0a316e669f783c3da55bbe9f31d5d92a77ba7be9
Alexander Trost <galexrt@googlemail.com>
2025-03-05 22:05:47 +0100

    fix: attempt relative pinia stores dir
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,11 @@
-import { createResolver } from "@nuxt/kit";
-const { resolve } = createResolver(import.meta.url);
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@vueuse/nuxt", "@pinia/nuxt", "pinia-plugin-persistedstate/nuxt"],
 
-  imports: {
-    dirs: [resolve("stores/**")],
+  pinia: {
+    storesDirs: [
+      '../stores/**',
+    ]
   },
 
   compatibilityDate: "2024-11-01",
