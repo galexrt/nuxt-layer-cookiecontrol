bcaffb2c8b552accd7b17ebb067282a99b0fa4fa
Alexander Trost <galexrt@googlemail.com>
2025-03-05 22:02:17 +0100

    fix: attempt import workaround for pinia
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,16 @@
+import { createResolver } from "@nuxt/kit";
+const { resolve } = createResolver(import.meta.url);
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@vueuse/nuxt',
-    '@pinia/nuxt',
-    'pinia-plugin-persistedstate/nuxt',
-  ],
-  compatibilityDate: '2024-11-01',
-  devtools: {
-    enabled: true
+  modules: ["@vueuse/nuxt", "@pinia/nuxt", "pinia-plugin-persistedstate/nuxt"],
+
+  imports: {
+    dirs: [resolve("stores/**")],
   },
-})
+
+  compatibilityDate: "2024-11-01",
+  devtools: {
+    enabled: true,
+  },
+});
