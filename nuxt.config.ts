b95d1cadd1f7a3ad13daee83764b2897834002de
Alexander Trost <galexrt@googlemail.com>
2025-03-05 22:07:00 +0100

    fix: use dirname attempt for pinia storesDir
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@vueuse/nuxt", "@pinia/nuxt", "pinia-plugin-persistedstate/nuxt"],
 
   pinia: {
     storesDirs: [
-      '../stores/**',
+      join(currentDir, './stores')+'/**',
     ]
   },
 
