ff4856d5a4cdeaff0ed53746744661cc2cac9b0b
Alexander Trost <galexrt@googlemail.com>
2025-03-05 21:58:05 +0100

    fix: adjust pinia stores dir
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
+    '@vueuse/nuxt',
     '@pinia/nuxt',
     'pinia-plugin-persistedstate/nuxt',
-    '@vueuse/nuxt',
   ],
   compatibilityDate: '2024-11-01',
   devtools: {
