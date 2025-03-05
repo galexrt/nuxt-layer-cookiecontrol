c84ec66573e3fb8eb5881429f86a7a0739521d6e
Alexander Trost <galexrt@googlemail.com>
2025-03-05 21:52:38 +0100

    fix: pinia store import
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- components/CookieControl.vue
+++ components/CookieControl.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { emojiBlast } from 'emoji-blast'
-import { useCookiesStore } from '~/store/cookies'
+import { useCookiesStore } from '../store/cookies'
 
 defineProps<{
   clearSiteDataURL?: string | undefined;
